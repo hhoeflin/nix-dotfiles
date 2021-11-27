967e8493911c45c8bda7eddd7c711da4410a00ba
Holger Hoefling <hhoeflin@gmail.com>
2021-11-27 17:43:18 +0100

    filename in lualine as relative

--- base/nvim/lua/init.lua
+++ base/nvim/lua/init.lua
@@ -2,7 +2,21 @@
 require('settings')
 
 -- lualine
-require('lualine').setup()
+require('lualine').setup(
+  {
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          file_status = true,  -- displays file status (readonly status, modified status)
+          path = 1,            -- 0 = just filename, 1 = relative path, 2 = absolute path
+          shorting_target = 40 -- Shortens path to leave 40 space in the window
+                               -- for other components. Terrible name any suggestions?
+        }
+      }
+    }
+  }
+)
 
 -- startify screen
 local alpha = require'alpha'
