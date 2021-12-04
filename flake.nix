61cf074e3e63badc3f41798b4f055e239b5a0013
Holger Hoefling <hhoeflin@gmail.com>
2021-12-04 21:34:44 +0100

    update hoeflho1 to 21.11

--- flake.nix
+++ flake.nix
@@ -58,7 +58,7 @@
           # You can update Home Manager without changing this value. See
           # the Home Manager release notes for a list of state version
           # changes in each release.
-          stateVersion = "21.05";
+          stateVersion = "21.11";
 
           configuration = { config, pkgs, ... }:
             let
