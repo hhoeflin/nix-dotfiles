1e3ff94224d72bb1c035c7a5a57746446aa08087
Holger Hoefling <hhoeflin@gmail.com>
2021-12-21 09:28:16 +0100

    use unstable channel

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "Home Manager NixOS configuration";
 
   inputs = {
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-21.05";
-    nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    #nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-21.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
