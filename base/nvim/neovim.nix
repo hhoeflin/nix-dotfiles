2958e7fb72729928d52e64bdd2b2e75f1b2e5ee6
Holger Hoefling <hhoeflin@gmail.com>
2022-04-24 23:26:04 +0200

    Update dependencies

--- base/nvim/neovim.nix
+++ base/nvim/neovim.nix
@@ -15,8 +15,8 @@
       src = pkgs.fetchFromGitHub {
         owner = "goolord";
         repo = "alpha-nvim";
-        rev = "23d90298d0ede40e80ed45db7acedc8981ae62e4";
-        sha256 = "1iqz5lhzs60468jldgq910dvirk56gcx55qg0cpvd3ga38mfbns5";
+        rev = "4781fcfea5ddc1a92d41b32dc325132ed6fce7a8";
+        sha256 = "sha256-GA+fIfVlHOllojGyErYGC0+zyYTl9rOxendqOgApJw4=";
       };
     };
     bufstop = pkgs.vimUtils.buildVimPlugin {
@@ -30,12 +30,12 @@
     };
 
     my-catppuccin-nvim = pkgs.vimUtils.buildVimPlugin {
-      name = "catpuccin";
+      name = "catppuccin";
       src = pkgs.fetchFromGitHub {
         owner = "catppuccin";
         repo = "nvim";
-        rev = "22b34eb9f93430bc010dee1523743b62cd2700fc";
-        sha256 = "sha256-ZcKHpQDcI2jNLhs5VYsVjAUpnM8Jwdo/1O0zo/k1DLM=";
+        rev = "8a67df6da476cba68ecf26a519a5279686edbd2e";
+        sha256 = "sha256-KSjPg/93YzprFyAFKOcGYZXlm2kjxMIYTZOeuu7OJvE=";
       };
     };
 
@@ -44,9 +44,9 @@
       src = pkgs.fetchFromGitHub {
         owner = "bluz71";
         repo = "vim-nightfly-guicolors";
-        rev = "d75a30e3874a9746d563634010accae4c463dc22";
+        rev = "2d8d5194ee45826a2994264e220d53546e608b4c";
         # sha256 = pkgs.lib.fakeSha256;
-        sha256 = "sha256-JhHjM9pyMnDd8Pkl5VWxn/aeY3b+oiBJh20wsIGYxLk=";
+        sha256 = "sha256-/l5ZIgD4etiQU5hSnVw06YZySReLNHiG/nrST0QZ0Zg=";
       };
     };
     neogen = pkgs.vimUtils.buildVimPluginFrom2Nix {
@@ -55,18 +55,11 @@
       src = pkgs.fetchFromGitHub {
         owner = "danymat";
         repo = "neogen";
-        rev = "218e0a63fd28fe3fcd34477e198193f26e0863ce";
+        rev = "f249a70ee598bdf8d015c10536c0dcd97c79b3aa";
         # sha256 = pkgs.lib.fakeSha256;
-        sha256 = "sha256-rKVQ7bO8d19GaVAF00FOOd8ORBNZmhGu0XuJR1n2FvU=";
+        sha256 = "sha256-x2jpJ49G6fJhwE2I2FA4wZy/moZokvApJhzUav+fzqA=";
       };
-      #src = pkgs.fetchurl {
-      #  url = "https://github.com/danymat/neogen/archive/d3e0168e1eb4c0a84b132fd0b554427e62a49552.tar.gz";
-      #  sha256 = "04ljl0jscff0xzrmd1ay0d78wjcs4xqc7fzcgkslimzm1yqpc71l";
-      #};
     };
-    #neogen = pkgs.vimUtils.buildVimPlugin {
-    #  name = "nvim-neogen";
-    #};
   in with pkgs.vimPlugins; [
     # various
     lualine-nvim
